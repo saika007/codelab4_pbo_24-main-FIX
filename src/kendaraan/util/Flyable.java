186672cf53cb0984d80d738d5c27f28f3dd61945
saika007 <ainulkafuur@gmail.com>
2024-04-08 02:30:48 +0700

    first commit

---
+++ src/kendaraan/util/Flyable.java
@@ -0,0 +1,5 @@
+package kendaraan.util;
+
+public interface Flyable {
+    public void Fly();
+}
---
+++ src/kendaraan/util/ShootAble.java
@@ -0,0 +1,5 @@
+package kendaraan.util;
+
+public interface ShootAble {
+    public void Shoot(String vehicle);
+}
